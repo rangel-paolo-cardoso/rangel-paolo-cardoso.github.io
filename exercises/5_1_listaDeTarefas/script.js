b6d9a0969c2333415edbd948801d254d9272c26d
Rangel Paolo <paolorangel1996@outlook.com>
2020-07-16 00:53:15 -0300

    Refatorada funçao que altera cor de fundo a pagina a cada 4 segundos.

--- exercises/5_1_listaDeTarefas/script.js
+++ exercises/5_1_listaDeTarefas/script.js
@@ -157,14 +157,12 @@ const eventBtnRemoveSelecionado = () =>
     document.querySelector('.selected').remove()
   );
 
-function alteraCor() {
-  const red = Number.parseInt(Math.random() * 255, 10);
-  const green = Number.parseInt(Math.random() * 255, 10);
-  const blue = Number.parseInt(Math.random() * 255, 10);
-  corpo.style.backgroundColor = `rgb(${red},${green},${blue})`;
-}
+const alteraCor = () => {
+  const color = () => parseInt(Math.random() * 255, 10);
+  corpo.style.backgroundColor = `rgb(${color()},${color()},${color()})`;
+};
 
-window.onload = function () {
+window.onload = () => {
   eventBtnAdiciona();
   eventBtnLimpa();
   eventBtnLimpaConcluidas();
