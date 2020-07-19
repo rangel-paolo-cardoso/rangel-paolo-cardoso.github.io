e6bfc25db95ed29203b859845d3ba6cb5918b32d
Rangel Paolo <paolorangel1996@outlook.com>
2020-07-19 15:00:05 -0300

    Refatorado codigo do quadrado-asterisc.js

--- exercises/4_2/quadrado-asterisc.js
+++ exercises/4_2/quadrado-asterisc.js
@@ -1,10 +1,5 @@
 let n = 5;
-let txt = '';
 
-for(let i = 0 ; i < n ; i++) {
-    txt += '*';
-}
-
-for(let i = 0 ; i < n ; i++) {
-    console.log(txt);
+for (let i = 0; i < n; i++) {
+  console.log([...Array(5)].fill('*').join(''));
 }
