95f729f384f5378302b5aed4216d43790fe63de1
Rangel Paolo <paolorangel1996@outlook.com>
2020-07-19 14:56:42 -0300

    Refatorado codigo da priramide-oca.js

--- exercises/4_2/priramide-oca.js
+++ exercises/4_2/priramide-oca.js
@@ -1,29 +1,19 @@
 let n = 7;
-let txt = [];
-let midle = ((n - 1) / 2) + 1;
+const txt = [];
+const midle = n / 2 + 0.5;
 
 txt.length = n;
-
-for(let i = 0; i < n ; i++) {
-    txt[i] = ' ';
-}
-
+txt.fill(' ');
 txt[midle - 1] = '*';
 
-for(let i = 0 ; i <= midle-1 ; i++) {
-    txt[midle-1-i] = '*';
-    txt[midle-1+i] = '*';
-    console.log(txt.join(''));
-    if(i == 0) {
-        txt[midle - 1] = ' ';
-    }
-    if(i > 0 && i < midle-1) {
-        txt[midle-1-i] = ' ';
-        txt[midle-1+i] = ' ';
-    }
-    if(i == midle-2) {
-        for(let i = 0; i < n ; i++) {
-            txt[i] = '*';
-        }
-    }
+for (let i = 0; i <= midle - 1; i++) {
+  txt[midle - 1 - i] = '*';
+  txt[midle - 1 + i] = '*';
+  console.log(txt.join(''));
+  if (i == 0) txt[midle - 1] = ' ';
+  if (i > 0 && i < midle - 1) {
+    txt[midle - 1 - i] = ' ';
+    txt[midle - 1 + i] = ' ';
+  }
+  if (i == midle - 2) txt.fill('*');
 }
