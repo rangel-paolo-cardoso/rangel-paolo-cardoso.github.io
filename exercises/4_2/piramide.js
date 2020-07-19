468e423900e07413ce73db4ff2f0629b6963b11e
Rangel Paolo <paolorangel1996@outlook.com>
2020-07-19 14:38:43 -0300

    Reatorado codigo da piramide.js

--- exercises/4_2/piramide.js
+++ exercises/4_2/piramide.js
@@ -1,17 +1,13 @@
 let n = 7;
-let txt = [];
-let midle = ((n - 1) / 2) + 1;
+const txt = [];
+const midle = n / 2 + 0.5;
 
 txt.length = n;
-
-for(let i = 0; i < n ; i++) {
-    txt[i] = ' ';
-}
-
+txt.fill(' ');
 txt[midle - 1] = '*';
 
-for(let i = 0 ; i <= midle-1 ; i++) {
-    txt[midle-1-i] = '*';
-    txt[midle-1+i] = '*';
-    console.log(txt.join(''));
+for (let i = 0; i <= midle - 1; i++) {
+  txt[midle - 1 - i] = '*';
+  txt[midle - 1 + i] = '*';
+  console.log(txt.join(''));
 }
