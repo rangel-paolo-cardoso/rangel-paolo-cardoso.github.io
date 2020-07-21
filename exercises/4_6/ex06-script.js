99a456d7901e1141146d23c8e45cec69463f25cf
Rangel Paolo <paolorangel1996@outlook.com>
2020-07-21 01:02:40 -0300

    Refatoradas funçoes do ex06 do bloco 4.6.

--- exercises/4_6/ex06-script.js
+++ exercises/4_6/ex06-script.js
@@ -1,25 +1,17 @@
-var personName = "";
+let personName = '';
 
-function complainMessage() {
-  alert("I told you not to do that " + personName);
-}
+const complainMessage = () => alert('I told you not to do that ' + personName);
 
-function typeAndScream() {
-  document.getElementById("audio").play();
-  personName = document.getElementById("name").value;
-}
+const typeAndScream = () => {
+  document.getElementById('audio').play();
+  personName = document.getElementById('name').value;
+};
 
-function goDark() {
-  var body = document.getElementsByTagName("body")[0];
-  if (body.className == "") {
-    body.className = "dark";
-  }
-  else {
-    body.className = "";
-  }
-  
-}
+const goDark = () => {
+  const body = document.getElementsByTagName('body')[0];
+  body.className = body.className == '' ? 'dark' : '';
+};
 
-function finishTheStory() {
-  document.getElementById("story").innerHTML = "So I carefully searched the damp dungeon. There you are! I grinned a smile with all 98 of my gleaming teeth. Dinner time!";
-}
+const finishTheStory = () =>
+  (document.getElementById('story').innerHTML =
+    'So I carefully searched the damp dungeon. There you are! I grinned a smile with all 98 of my gleaming teeth. Dinner time!');
