691d743dcbbc471389df6cafe09d6921de7f7140
Rangel Paolo <paolorangel1996@outlook.com>
2020-07-24 23:49:12 -0300

    Reafatorado codigo 8.2 ex01part1.js

--- exercises/8_2/ex01part1.js
+++ exercises/8_2/ex01part1.js
@@ -1,8 +1,8 @@
-var testObj = {
+const testObj = {
   12: "Namath",
   16: "Montana",
   19: "Unitas"
 };
 
-var playerNumber = 16;
-var player = testObj[playerNumber];
+const playerNumber = 16;
+const player = testObj[playerNumber];
