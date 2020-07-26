84a1086efb2a0cfb39f2ab901386402328a3693a
Rangel Paolo <paolorangel1996@outlook.com>
2020-07-26 03:19:03 -0300

    Refatorado codigo 8.3 ex05.js

--- exercises/8_3/ex05.js
+++ exercises/8_3/ex05.js
@@ -1,16 +1,17 @@
 const assert = require('assert');
 
-function myRemoveWithoutCopy(arr, item) {
-  for (let i = 0, len = arr.length; i < len; i += 1) {
-    if (arr[i] === item) {
-      arr.splice(i, 1);
-      i -= 1;
-      len -= 1;
-    }
-  }
+const myRemoveWithoutCopy = (arr, item) =>
+  (arr = arr.filter((num) => num !== item));
+  
+  // for (let i = 0, len = arr.length; i < len; i += 1) {
+  //   if (arr[i] === item) {
+  //     arr.splice(i, 1);
+  //     i -= 1;
+  //     len -= 1;
+  //   }
+  // }
 
-  return arr;
-}
+  // return arr;
 
 assert.deepEqual(myRemoveWithoutCopy([1, 2, 3, 4], 3), [1, 2, 4]);
 assert.notEqual(myRemoveWithoutCopy([1, 2, 3, 4], 3), [1, 2, 3, 4]);
