475e661561dd745c9a0dd8c2920b697d4b625d53
Rangel Paolo <paolorangel1996@outlook.com>
2020-07-27 02:28:17 -0300

    Reafotado codigo bloco 8.4 ex05.js

--- exercises/8_4/ex05.js
+++ exercises/8_4/ex05.js
@@ -1,13 +1,13 @@
 const assert = require('assert');
 
-const greetPeople = (people) => {
-  let greeting = [];
+const greetPeople = (people) => people.map((person) => `Hello ${person}`);
+  // let greeting = [];
 
-  for (const person in people) {
-    greeting.push(`Hello ${people[person]}`);
-  }
-  return greeting;
-};
+  // for (const person in people) {
+  //   greeting.push(`Hello ${people[person]}`);
+  // }
+  // return greeting;
+
 
 const parameter = ['Irina', 'Ashleigh', 'Elsa'];
 const result = ['Hello Irina', 'Hello Ashleigh', 'Hello Elsa'];
