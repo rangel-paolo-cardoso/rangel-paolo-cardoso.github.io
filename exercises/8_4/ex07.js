0426ca5c3b147c54439d6eba54249432b13d9ed0
Rangel Paolo <paolorangel1996@outlook.com>
2020-07-27 02:48:33 -0300

    Reafotado codigo bloco 8.4 ex07.js

--- exercises/8_4/ex07.js
+++ exercises/8_4/ex07.js
@@ -1,14 +1,13 @@
 const assert = require('assert');
 
-const greaterThanTen = (array) => {
-  let results = [];
-  for (let i = 0; i < array.length; i += 1) {
-    if (array[i] > 10) {
-      results.push(array[i]);
-    }
-  }
-  return results;
-};
+const greaterThanTen = (array) => array.filter((num) => num > 10);
+  // let results = [];
+  // for (let i = 0; i < array.length; i += 1) {
+  //   if (array[i] > 10) {
+  //     results.push(array[i]);
+  //   }
+  // }
+  // return results;
 
 const parameter = [4, 10, 32, 9, 21];
 const result = [32, 21];
