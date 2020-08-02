7de06715ac1e56efc636b0779f584fc18c0e6559
Rangel Paolo <paolorangel1996@outlook.com>
2020-08-02 02:33:50 -0300

    Arrumado codigo ex07.js bloco 9.1

--- exercises/9_1/ex07.js
+++ exercises/9_1/ex07.js
@@ -65,6 +65,6 @@ const books = [
 
 const expected_result = true
 
-const someBookWasReleaseOnThe80s = () => books.some(book => book.releaseYear >= 1980 && book.releaseYear < 1990);
+const someBookWasReleaseOnThe80s = () => books.some((book) => book.releaseYear >= 1980 && book.releaseYear < 1990);
 
 assert.equal(someBookWasReleaseOnThe80s(), expected_result);
