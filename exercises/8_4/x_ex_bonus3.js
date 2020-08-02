fc6e977aa97f88b140e84865acf98346abb2e1f6
Rangel Paolo <paolorangel1996@outlook.com>
2020-08-02 02:03:48 -0300

    Refatorado código x_ex_bonus3.js bloco 8.4.

--- exercises/8_4/x_ex_bonus3.js
+++ exercises/8_4/x_ex_bonus3.js
@@ -1,14 +1,13 @@
 const assert = require('assert');
 
-const getLargestNumber = (array) => {
-  let largestNumber = array[0];
-  for (let i = 0; i < array.length; i += 1) {
-      if (array[i] > largestNumber) {
-          largestNumber = array[i];
-      }
-  }
-  return largestNumber;
-}
+const getLargestNumber = (array) => array.reduce((res, nxt) => (res = res > nxt ? res : nxt), 0);
+// let largestNumber = array[0];
+// for (let i = 0; i < array.length; i += 1) {
+//     if (array[i] > largestNumber) {
+//         largestNumber = array[i];
+//     }
+// }
+// return largestNumber;
 
 const parameter = [45, 8, 2, 50, 1, 7, 99];
 const result = 99;
