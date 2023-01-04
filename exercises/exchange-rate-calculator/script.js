0f624ec40d81fae54ffdafce057d25700274aadf
Rangel Paolo Cardoso Bomfim <paolorangel1996@outlook.com>
2023-01-04 17:34:06 -0300

    Changed profile photo to Polish's one

--- exercises/exchange-rate-calculator/script.js
+++ exercises/exchange-rate-calculator/script.js
@@ -1,4 +1,21 @@
-const select = document.getElementById('currency-one');
+const currencyEl_one = document.getElementById('currency-one');
+const amountEl_one = document.getElementById('amount-one');
+const currencyEl_two = document.getElementById('currency-two');
+const amountEl_two = document.getElementById('amount-two');
 
-select.innerHTML = currenctInitials.countryInitials;
-alert();
+const rate = document.getElementById('rate');
+const swap = document.getElementById('swap');
+
+// Fecth exchange rates and update the DOM
+function calculate() {
+  const currency_one = currencyEl_one.value;
+  const currency_two = currencyEl_two.value;
+}
+
+// Event listeners
+currencyEl_one.addEventListener('change', calculate);
+amountEl_one.addEventListener('input', calculate);
+currencyEl_two.addEventListener('change', calculate);
+amountEl_two.addEventListener('input', calculate);
+
+calculate();
